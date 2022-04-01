ce8f248220ea7e4d422870ebe4c4e90904fa6a52
Philip Kerschner <pkerschner@zoominternet.net>
2022-03-31 22:14:34 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-cv-angular';
+    name: string = "Philip Kerschner";
+    contact: string = "Address | Phone | Email"
+    technologies: string[] = [
+      "C#", "Java", "SQL", "Git", "GitHub", "EF", "Spring", 
+      "WebAPI", "HTML", "CSS", "Bootstrap", "TypeScript", "JavaScript", "Angular", 
+      "Visual Studio"
+    ];
+    workHistories = [
+      {
+        role: "Associate Contract Administrator",
+        duration: "2018 to 2021",
+        accomplishments: [
+          "Managed contracts",
+          "Submitted quotes"
+        ]
+      },
+      {
+        role: "Assembler",
+        duration: "2015 to 2018",
+        accomplishments: [
+          "Developed processes",
+          "Assembled and tested products"         
+        ]
+      }
+    ];
+    education = [
+      {
+        school: "North Central State College",
+        degree: "Business"
+      }
+    ];
 }
